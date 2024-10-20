b651c6e972498c9317cf8f11df12a044537fa6f4
Nguyễn Việt <nqviet2705@gmail.com>
2024-10-20 15:04:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const noBtn = document.querySelector(".no-btn");
 const questionPrompt = document.querySelector(".question-prompt"); // Thêm đoạn này
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Anh đúng là đồ tòyyyyyyy 🥺<br>dỗiiii x10000000";
+  question.innerHTML = "Hoy ma cho a xin lui, cho a no qua nhe dao nay a ban qua T.T";
   gif.src =
     "https://media.giphy.com/media/vvc1dJPLEU2QOw9cXy/giphy.gif";
 
